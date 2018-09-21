fc7ee68c3a50cf7b83eed9f1c06d02e968ed3e04
marcinlecki <marcinlecki@poczta.onet.pl>
2018-09-21 15:28:43 +0200

    new picking
    New conception for picking in matplotlib

--- pick.py
+++ pick.py
@@ -1,4 +1,4 @@
-# -*- coding: utf-8 -*-
+#-*- coding: utf-8 -*-
 """
 Created on Mon Aug 07 12:58:45 2017
 @author: Marcin
@@ -20,13 +20,34 @@ img=mpimg.imread('Saboya_Re_648.png')
 fig= plt.figure()
 ax=fig.add_subplot(111)
 
+#loading an image
 ax.imshow(img)
 
+
+
+#store the points
 xy=[]
 
 
 #table with rectangles
 rects=[]
+
+
+class recti:
+    def __init__(self, x0, y0, dx, dy, axes):
+        self.x0=x0
+        self.y0=y0
+        self.dx=dx
+        self.dy=dy
+        self.line=[0,0,0,0]
+        self.line[0],=axes.plot([x0,x0 + dx],[y0, y0], picker=5)
+        self.line[1],=axes.plot([x0 + dx, x0 + dx], [y0, y0 + dy], picker=5)
+        self.line[2],=axes.plot([x0 + dx, x0], [y0+dy, y0+dy], picker=5)
+        self.line[3],=axes.plot([x0, x0], [y0+dy, y0], picker=5)
+    
+   
+
+
 
 
 #kiedy zaznacza się dane na wykresie
@@ -41,111 +62,28 @@ def onclick(event):
         ax.add_patch(rect)
         fig.canvas.draw()
     else:
-        x=event.x
-        y=event.y
-        rects.append(RectArea(x,y))
-        for rect in rects:
-            rect.draw(fig, ax)
+        x=event.xdata
+        y=event.ydata
+        recti0=recti(x,y,100.0,100.0, ax)
+        rects.append(recti0)
+        fig.canvas.draw()
         
-    
-    
-
-
-width=0.0
-height=0.0
-
-
-#def on_press(self, event):
-    
-
-class RectArea:
-    #because rectangle or circle were not added to figure a priori - figure and axes must be passed
-    def __init__(self, xi,yi):
-        self.width=20
-        self.height=10
-        self.x0=xi
-        self.y0=yi
-        self.edgecol="red"
-        self.r=4
-        #angle is anticlockwise
-        self.angle=0.0
-        #storing the background of rectangle
-        self.background = None
-        #storing the backgroud of circle
-        self.backgroundc=None
-        #indicator of the selection of the rectangle - creating an instance makes it selected
-        self.is_Recselected=True
-        #indicator of the selection of circle
-        self.is_Circselected=False
-        self.rect = Rectangle((self.x0,self.y0), self.width, self.height, ec=self.edgecol, fill=False, ls="solid")
-        #upper right circle - draggable
-        self.xr=self.x0+self.width
-        self.yr=self.y0+self.height
-        self.circle=Circle((self.xr, self.yr), radius=self.r)
-        #circle - origin of the rectangle -> left, bottom corner of the rectangle 
-        self.circle_o=Circle((self.x0, self.y0), radius=self.r)
-        self.rect.set_animated(True)
-        self.circle.set_animated(True)
         
-    
         
-    def draw(self, fig, axes):
-        canvas=fig.canvas
-        canvas.draw()
-        self.background=canvas.copy_from_bbox(axes.bbox)
-        axes.draw_artist(self.rect)
-        axes.draw_artist(self.circle)
-        axes.draw_artist(self.circle_o)
-        canvas.blit(axes.bbox)
-        
-'''     
-     
-    def clickRect(self):
-        #clicking rectangle makes it selected
-        canvas=self.rect.figure.canvas
-        axes=self.rect.axes
-        self.rect.set_animated(True)
-        #clicking the circle makes it draggable
-        
-     
-    def dragRect(self, x_mouse, y_mouse):
-        w=x_mouse-self.x0
-        h=y_mouse-self.y0
-        self.rect.set_width(w)
-        self.rect.set_height(h)
-        #drawing of the rectangle with blit technique 
-        canvas=self.rect.figure.canvas
-        axes=self.rect.axes
-        
-        canvas.draw()
-        canvas.restore_region()
-    
-    def releaseRect(self):
-        pass
-        
-    def connect(self):
-        'connect to all the events we need'
-        self.cidpress = self.rect.figure.canvas.mpl_connect(
-            'button_press_event', self.clickRect)
-        self.cidrelease = self.rect.figure.canvas.mpl_connect(
-            'button_release_event', self.releaseRect)
-        self.cidmotion = self.rect.figure.canvas.mpl_connect(
-            'motion_notify_event', self.dragRect)
+def onMove(event):
+    for rectii in rects:
+        for i in range (4):
+           print(rectii.line[i].axes)
+           if event.inaxes == rectii.line[i].axes:
+               rectii.line[i].set_linestyle("--")
+               fig.canvas.draw()
    
-   #disconnection
-    def disconnect(self):
-        'disconnect all the stored connection ids'
-        self.rect.figure.canvas.mpl_disconnect(self.cidpress)
-        self.rect.figure.canvas.mpl_disconnect(self.cidrelease)
-        self.rect.figure.canvas.mpl_disconnect(self.cidmotion)
-       
-           
+
+
+
         
-'''
+        
     
-
-
-
-
 cidpress = fig.canvas.mpl_connect('button_press_event', onclick)
-  
+
+fig.canvas.mpl_connect('motion_notify_event', onMove)
